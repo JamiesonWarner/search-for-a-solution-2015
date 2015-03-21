d35ef02417a14c5c1d9cb44695eafae196c7aa4e
Dave Sescleifer <dave@sescleifer.com>
2015-03-21 18:36:49 -0500

    Some template stuff

--- core/views.py
+++ core/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from .models import Project
 
 def index(request):
-  projects = Project.objects.all()
+  projects = Project.objects.prefetch_related('wanted_set').all()
   return render(request, 'index.html', { 'projects': projects })
