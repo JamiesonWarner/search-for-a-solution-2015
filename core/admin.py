117be7d3d872157618d9591580eb3646d2dfa0b4
Dave Sescleifer <dave@sescleifer.com>
2015-03-21 18:06:21 -0500

    Registered project

--- core/admin.py
+++ core/admin.py
@@ -3,6 +3,7 @@ from .models import *
 
 admin.site.register(Tag)
 admin.site.register(Role)
+admin.site.register(Project)
 admin.site.register(Student)
 admin.site.register(Wanted)
 admin.site.register(Comment)
