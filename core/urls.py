cc79e892e7b5a48b28b77d9da0770c703c583ebe
Dave Sescleifer <dave@sescleifer.com>
2015-03-21 19:19:54 -0500

    Project details page

--- core/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
-
-urlpatterns = patterns('',
-    url(r'^$/', 'core.views.index'),
-    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-)
--- wiscollab/urls.py
+++ wiscollab/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^$', 'core.views.index'),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', 'core.views.index'),
+    url(r'^(\d+)$', 'core.views.project_details'),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 )
