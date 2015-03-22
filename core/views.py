3fdf8956c470b7d1372230253b759a7fec94a3a9
Dave Sescleifer <dave@sescleifer.com>
2015-03-21 19:01:03 -0500

    Stuff

--- core/views.py
+++ core/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Project
 
 def index(request):
   projects = Project.objects.prefetch_related('wanted_set').all()
   return render(request, 'index.html', { 'projects': projects })
+
+def project_details(request, project_id):
+  project = get_object_or_404(Project, pk=project_id)
+  return render(request, 'project_details.html', { 'project': project })
